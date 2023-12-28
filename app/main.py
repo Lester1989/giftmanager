5a55721e888badf8f24dbbc276a154a18b683e0d
Lester1989 <lester@techfak.uni-bielefeld.de>
2023-12-28 19:13:08 +0100

    add friend and delete friend wired from overview

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI,Request
+from datetime import datetime
+from typing import Optional
+from fastapi import FastAPI, Form,Request,status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -33,6 +35,22 @@ def home(request: Request):
 def get_friends(request: Request):
     friends = db.session.query(Friend).all()
     return templates.TemplateResponse("friend_overview.html", {"request": request, "friends": friends})
+
+@app.post("/add_friend", response_class=RedirectResponse)
+def add_friend(request: Request,first_name: str=Form(), last_name: Optional[str] = Form(""), address: Optional[str] = Form(""), phone_number: Optional[str] = Form(""), email: str = Form(""), birthday: datetime = Form(datetime.now()), notes: Optional[str] = Form(""), receives_christmas_gift: Optional[bool] = Form(False), receives_birthday_gift: Optional[bool] = Form(False)):
+    friend = Friend(id=str(new_ulid()), first_name=first_name, last_name=last_name, address=address, phone_number=phone_number, email=email, birthday=birthday, notes=notes, receives_christmas_gift=receives_christmas_gift, receives_birthday_gift=receives_birthday_gift)
+    db.session.add(friend)
+    db.session.commit()
+    return RedirectResponse(url='/friends', status_code=status.HTTP_302_FOUND)
+
+@app.get("/delete_friend/{friend_id}", response_class=RedirectResponse)
+def delete_friend(request: Request, friend_id: str):
+    friend = db.session.query(Friend).get(friend_id)
+    if not friend:
+        raise HTTPException(status_code=404, detail="Friend not found")
+    db.session.delete(friend)
+    db.session.commit()
+    return RedirectResponse(url='/friends', status_code=status.HTTP_302_FOUND)
 
 @app.get("/friends/{friend_id}", response_class=HTMLResponse)
 def get_friend(request: Request, friend_id: str):
