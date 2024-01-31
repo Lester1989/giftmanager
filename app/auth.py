01faca5b1d441b3cc2851d6249074fd3e8d7ef1d
Lester1989 <lester@techfak.uni-bielefeld.de>
2024-01-31 21:42:05 +0100

    fix password decoding and encoding

--- app/auth.py
+++ app/auth.py
@@ -28,16 +28,16 @@ from fastapi.security.utils import get_authorization_scheme_param
 import bcrypt
 
 # Hash a password using bcrypt
-def hash_password(password):
+def hash_password(password:str):
     pwd_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
-    return hashed_password
+    return hashed_password.decode('utf-8')
 
 # Check if the provided password matches the stored password (hashed)
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password:str, hashed_password:str):
     password_byte_enc = plain_password.encode('utf-8')
-    return bcrypt.checkpw(password = password_byte_enc , hashed_password = hashed_password)
+    return bcrypt.checkpw(password = password_byte_enc , hashed_password = hashed_password.encode('utf-8'))
 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 180
